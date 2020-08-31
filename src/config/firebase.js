6b65d03590da1618e25376dde4f08e18cf9373ce
abdoug <o.abderrahmane@kyo-conseil.com>
2020-08-31 22:34:53 +0100

    setting up the header

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB2xY8r42fQiXES6086PCQUsMTSD2zTbYE",
+  authDomain: "facebook-clone-6d013.firebaseapp.com",
+  databaseURL: "https://facebook-clone-6d013.firebaseio.com",
+  projectId: "facebook-clone-6d013",
+  storageBucket: "facebook-clone-6d013.appspot.com",
+  messagingSenderId: "516618223309",
+  appId: "1:516618223309:web:9b0062dde89994e05c7384",
+  measurementId: "G-FE7NCLZCKY"
+};
